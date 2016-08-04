181e3d4441561ba5f9290b494ceb5bc158781a39
zhangcong <zhangcong@kingsoft.com>
2016-08-04 11:22:23 +0800

    client

--- TcpServer/src/test/java/com/iot/tcpserver/client/ClientMain.java
+++ TcpServer/src/test/java/com/iot/tcpserver/client/ClientMain.java
@@ -29,22 +29,26 @@ public class ClientMain {
         ClientEnv.AES_KEY = CryptUtil.generateAESKey();
     }
 
+    private static final boolean IS_APP = true;
     private static ChannelHandler<ClientSocketChannel,BaseMsg> handler = new ChannelHandler<ClientSocketChannel,BaseMsg>() {
 
         private final BaseMsg HEARTBEAT_MSG = new BaseMsg(ServerEnv.CMD_HEARTBEAT,null);
 
+        //对于一些没有RSA计算能力的设备,不进行密钥协商,直接doAuth
         @Override
         public void onRead(ClientSocketChannel ctx, BaseMsg msg)throws Exception {
             System.out.println("======client recv::::" + msg.toString());
             switch (msg.getCmd()){
                 case ServerEnv.CMD_PUSH_RSA_PUB_KEY:
-                    byte[] b = CryptUtil.rsaEncryptByPublicKey(ClientEnv.AES_KEY,new String(msg.getData(),"UTF-8"));
-                    ctx.send(new BaseMsg(ServerEnv.CMD_SEND_AES_KEY,b));
+                    if(IS_APP){
+                        byte[] b = CryptUtil.rsaEncryptByPublicKey(ClientEnv.AES_KEY,new String(msg.getData(),"UTF-8"));
+                        ctx.send(new BaseMsg(ServerEnv.CMD_SEND_AES_KEY,b));
+                    }
                     break;
                 case ServerEnv.CMD_SEND_AES_KEY://resp
                     if(msg.getData()!=null && msg.getData().length==1 && msg.getData()[0]==1){
                         //1代表密钥协商成功,成功后进行认证
-                        doAuth(ctx);
+                        doAppAuth(ctx);
                     }else{
                         //TODO handle error
                     }
@@ -60,6 +64,13 @@ public class ClientMain {
         @Override
         public void onConnected(ClientSocketChannel ctx){
             System.out.println("----onConnected----");
+            if(!IS_APP){//device
+                try {
+                    doDeviceAuth(ctx);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
         }
 
         @Override
@@ -67,13 +78,19 @@ public class ClientMain {
             System.out.println("----onClosed----");
         }
 
-        private void doAuth(ClientSocketChannel ctx) throws Exception {
+        private void doAppAuth(ClientSocketChannel ctx) throws Exception {
             JSONObject json = new JSONObject();
             json.put("version","1.0");
-            json.put("id","123456789");
+            json.put("id","app123456789");
             json.put("username","zc_username");
             json.put("password",CryptUtil.md5("zc_password"));
             ctx.send(new BaseMsg(ServerEnv.CMD_APP_AUTH,true,json.toJSONString().getBytes("UTF-8")));
+        }
+        private void doDeviceAuth(ClientSocketChannel ctx) throws Exception {
+            JSONObject json = new JSONObject();
+            json.put("version","1.0");
+            json.put("id","device123456789");
+            ctx.send(new BaseMsg(ServerEnv.CMD_APP_AUTH,false,json.toJSONString().getBytes("UTF-8")));
         }
     };
 

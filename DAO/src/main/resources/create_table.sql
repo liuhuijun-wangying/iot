490080437b11ae1efbb0142023561d9b87bb8d6e
zhangcong <zhangcong@kingsoft.com>
2016-08-11 15:07:30 +0800

    update user sql

--- DAO/src/main/resources/create_table.sql
+++ DAO/src/main/resources/create_table.sql
@@ -2,10 +2,11 @@
 -- Table structure for `user`
 -- ----------------------------
 CREATE TABLE IF NOT EXISTS `user` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(50) NOT NULL,
   `password` varchar(32) NOT NULL,
-  `group` varchar(50) DEFAULT NULL,
+  `userGroup` varchar(50) DEFAULT NULL,
   `extraInfo` varchar(255) DEFAULT NULL,
+  `regTime` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;

9041c05abffe728a53bacdf45c5d9f85df5b60d0
zhangcong <zhangcong@kingsoft.com>
2016-08-27 17:19:59 +0800

    modify run script

--- run-dev.sh
+++ run-dev.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 #仅用于测试
-#stop zk
-#sudo ./Zookeeper/bin/zkServer.sh stop
-#stop kafka
-#sudo ./Kafka/bin/kafka-server-stop.sh
+#It is single mode instead of cluster mode
 
 #start zk if not started
 zk_status=$(sudo ./Zookeeper/bin/zkServer.sh status)
@@ -12,6 +9,8 @@ if [[ $zk_status =~ "Error contacting service" ]]; then
     echo "======>:starting zk server..."
     sudo ./Zookeeper/bin/zkServer.sh start
 fi
+#stop zk
+#sudo ./Zookeeper/bin/zkServer.sh stop
 
 #start kafka if not started
 kafka_status=$(ps ax | grep -i 'kafka\.Kafka' | grep java | grep -v grep | awk '{print $1}')
@@ -25,33 +24,20 @@ if [ -z "$kafka_status" ]; then
     sudo ./Kafka/bin/kafka-topics.sh --if-not-exists --create --zookeeper localhost:2181 --replication-factor 1 --partitions 3 --topic topic-service-resp
     sudo ./Kafka/bin/kafka-topics.sh --if-not-exists --create --zookeeper localhost:2181 --replication-factor 1 --partitions 3 --topic topic-service
 fi
+#stop kafka
+#sudo ./Kafka/bin/kafka-server-stop.sh
 
 #build java project
 mvn clean package -Dmaven.test.skip=ture
 
-#start dispatcher
-dispatcher_status=$(ps ax | grep nohup | grep -i 'Dispatcher' | grep java | grep -v grep | awk '{print $1}')
-if [ -n "$dispatcher_status" ]; then
-    echo "======>:killing dispatcher server"
-    sudo kill -s TERM $dispatcher_status
-fi
-echo "======>:starting dispatcher server"
-sudo nohup java -jar ./Dispatcher/target/Dispatcher-1.0.0-jar-with-dependencies.jar >/dev/null 2>&1 &
-
-#start tcp server
-tcpserver_status=$(ps ax | grep nohup | grep -i 'TcpServer' | grep java | grep -v grep | awk '{print $1}')
-if [ -n "$tcpserver_status" ]; then
-    echo "======>:killing tcp server"
-    sudo kill -s TERM $tcpserver_status
-fi
-echo "======>:starting tcp server"
-sudo nohup java -jar ./TcpServer/target/TcpServer-1.0.0-jar-with-dependencies.jar >/dev/null 2>&1 &
-
-#start service
-common_status=$(ps ax | grep nohup | grep -i 'CommonService' | grep java | grep -v grep | awk '{print $1}')
-if [ -n "$common_status" ]; then
-    echo "======>:killing common service"
-    sudo kill -s TERM $common_status
-fi
-echo "======>:starting common service"
-sudo nohup java -jar ./CommonService/target/CommonService-1.0.0-jar-with-dependencies.jar >/dev/null 2>&1 &
+#restart server modules in single mode
+modules=("Dispatcher" "TcpServer" "CommonService")
+for module in ${modules[@]}; do
+    status=$(ps ax | grep nohup | grep ${module} | grep java | grep -v grep | awk '{print $1}')
+    if [ -n "$status" ]; then
+        echo "======>:killing ${module}"
+        sudo kill -s TERM $status
+    fi
+    echo "======>:starting ${module}"
+    sudo nohup java -jar ./${module}/target/${module}-1.0.0-jar-with-dependencies.jar >/dev/null 2>&1 &
+done
